a1bb1e6783fa96faa6781cd41c107bd6fcd340bf
pa1b <31280326+pa1b@users.noreply.github.com>
2017-08-26 19:24:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,8 +97,9 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/counter', function(req,res) {
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 
