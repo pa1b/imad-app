bddc1671996ec64cbf2b19850fdae41f903facaa
pa1b <31280326+pa1b@users.noreply.github.com>
2017-08-26 13:48:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,12 +77,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
+/*app.get('/article-one',function(req,res){
     res.send(createTemplate(articles['article-one']));
 });
 app.get('/article-two',function(req,res){
     res.send(createTemplate(articles['article-two']));
+}); */
+
+app.get('/:articleName',function(req,res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
