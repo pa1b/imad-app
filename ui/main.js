d00f8c82758f77a7095ead9384ba457a93d1e9a9
pa1b <31280326+pa1b@users.noreply.github.com>
2017-08-27 18:37:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,14 +44,14 @@ submit.onclick = function () {
                   }
     var ul = document.getElementById ('namelist');
     ul.innerHTML = list;
-};
+}
           }
       }
   };
   //Make Request
   var nameInput = document.getElementById('name');
-  var  name = nameInput.value;
-  request.open('GET','http://pavanbellamkonda98.imad.hasura-app.io/submit-name?name' + name,true);
+  var name = nameInput.value;
+  request.open('GET','http://pavanbellamkonda98.imad.hasura-app.io/submit-name?name=' + name,true);
   request.send(null);
 
   //Capture a list of names and render it
