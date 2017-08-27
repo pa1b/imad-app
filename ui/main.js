8a66d215055998659c5f22d64eda050100386f8a
pa1b <31280326+pa1b@users.noreply.github.com>
2017-08-27 18:42:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,11 +42,11 @@ submit.onclick = function () {
                           list += '<li>' + names[i] +'</li>';
         
                   }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-}
+                  var ul = document.getElementById('namelist');
+                  ul.innerHTML = list;
+                }
           }
-      }
+      };
   };
   //Make Request
   var nameInput = document.getElementById('name');
